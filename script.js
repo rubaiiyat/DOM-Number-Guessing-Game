763def0807957959867abccc849bcf7ae23fa98e
rubaiiyat <abirr1516@gmail.com>
2024-11-08 13:08:05 +0600

    Guess Then Number Game complete

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let randomNum;
+let lifeAmount = 10;
+let balanceAmount = 10;
+
 document.getElementById("generateBtn").addEventListener("click", function () {
   const generated = document.getElementById("notGenerated");
   randomNum = Math.round(Math.random() * 100) + 1;
@@ -21,20 +24,46 @@ inputValue.addEventListener("input", function () {
 
 document.getElementById("checkBtn").addEventListener("click", function () {
   let myInput = parseInt(allValue);
-  if (isNaN(myInput)) {
-    console.log("Please Enter Valid Value");
-    inputValue.value = "";
-    allValue = "";
-  } else {
-    if (myInput == randomNum) {
-      console.log("Your are corrent");
-    } else if (randomNum > myInput) {
-      console.log(`The Number is Greater Than ${myInput}`);
-    } else {
-      console.log(`The Number is Less Than ${myInput}`);
-    }
 
-    inputValue.value = "";
-    allValue = "";
+  if (myInput == randomNum) {
+    const notGenerated = document.getElementById("notGenerated");
+    notGenerated.innerText = "Your Answer is right and your got $5";
+    balanceAmount += 5;
+  } else if (randomNum > myInput) {
+    const greaterThen = document.getElementById("greaterThen");
+    const newP = document.createElement("p");
+    newP.innerHTML = `
+    
+    <p>Your Value is Greater Than ${myInput}</p>
+    `;
+
+    greaterThen.appendChild(newP);
+    lifeAmount -= 1;
+  } else {
+    const lessThen = document.getElementById("lessThen");
+    const newP = document.createElement("p");
+    newP.innerHTML = `
+    
+    <p>Your Value is Less Than ${myInput}</p>
+    `;
+
+    lessThen.appendChild(newP);
+    lifeAmount -= 1;
+  }
+
+  inputValue.value = "";
+  allValue = "";
+
+  const life = document.getElementById("life");
+  life.innerText = `Life: ${lifeAmount}`;
+
+  const balance = document.getElementById("balance");
+  balance.innerText = `Balance: $${balanceAmount}`;
+
+  const disableBtn = document.getElementById("checkBtn");
+
+  if (lifeAmount <= 0) {
+    disableBtn.style.cursor = "not-allowed";
+    disableBtn.disabled = true;
   }
 });
